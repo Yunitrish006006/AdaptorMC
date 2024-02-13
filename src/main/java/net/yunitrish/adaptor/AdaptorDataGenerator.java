292a52e14de30147557aad563c957f12d5b1b911
Yunitrish <yunthomas006@gmail.com>
2024-02-14 04:24:14 +0800

    init

---
+++ src/main/java/net/yunitrish/adaptor/AdaptorDataGenerator.java
@@ -0,0 +1,11 @@
+package net.yunitrish.adaptor;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AdaptorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
